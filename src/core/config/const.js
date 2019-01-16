a65582115d7012dced1bf6febfe4f7495eaec096
to-day <1499128252@qq.com>
2019-01-16 16:22:48 +0800

    12

--- src/core/config/const.js
+++ src/core/config/const.js
@@ -14,5 +14,5 @@ export const MapAPI = {
 //   css: "http://localhost/arcgis_js_v48_sdk/arcgis_js_v48_sdk/arcgis_js_api/library/4.8/esri/css/main.css"
 // };  //天翔
 
-// export const IP = "122.112.216.247:8080"; //正式
-export const IP = "192.168.1.109:8080"; //测试
+export const IP = "122.112.216.247:8080"; //正式
+// export const IP = "192.168.1.109:8080"; //测试
