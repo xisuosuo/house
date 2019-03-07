1e3289c3b42d150a1fd0aef078d62b70ca557dc1
xisuosuo <1315270706@qq.com>
2019-03-07 18:22:20 +0800

    吴天翔
    吴天翔

--- src/views/houseCompare/components/houseinfo.vue
+++ src/views/houseCompare/components/houseinfo.vue
@@ -1,76 +1,228 @@
 <template>
-    <div style="height: 1000px;margin-top: 60px;width: 100%">
-        <div style="border: 1px solid #dcdee2;width: 900px;margin: 0 auto">
-            <Tabs value="name1">
-                <TabPane label="楼盘首页" name="name2">
-                </TabPane>
-                <TabPane label="详细信息" name="name1">
-                    <div style="padding: 14px">
-                        <h3>基本信息</h3>
-                        <table class="table-noline">
-                            <tbody>
-                            <tr>
-                                <td class="label-l">楼盘名称：</td>
-                                <td valign="top" class="text-l"> 海上明珠</td>
-                                <td class="label-r">建成年代：</td>
-                                <td valign="top" class="text-r">暂无数据</td>
-                            </tr>
-                            <tr>
-                                <td class="label-l">物业类型：</td>
-                                <td valign="top" class="text-l"> 普通住宅 , 自住型商品房</td>
-                                <td class="label-r">项目特色：</td>
-                                <td valign="top" class="text-r"> 投资地产 , 河景地产 , 名企盘</td>
-                            </tr>
-                            <tr>
-                                <td class="label-l">建筑类型：</td>
-                                <td valign="top" class="text-l"> 板楼 , 高层</td>
-                                <td class="label-r">装修标准：</td>
-                                <td valign="top" class="text-r">暂无数据</td>
-                            </tr>
-                            <tr>
-                                <td class="label-l">产权年限：</td>
-                                <td valign="top" class="text-l">70年</td>
-                                <td class="label-r">装修情况：</td>
-                                <td valign="top" class="text-r"> 毛坯</td>
-                            </tr>
-                            <tr>
-                                <td class="label-l">开发商：</td>
-                                <td valign="top" class="text-l"> 烟台海上明珠置业有限公司</td>
-                                <td class="label-r">环线位置：</td>
-                                <td valign="top" class="text-r">暂无数据</td>
-                            </tr>
-                            <tr>
-                                <td class="label-l">投资商：</td>
-                                <td valign="top" class="text-l"> 暂无数据</td>
-                            </tr>
-                            <tr>
-                                <td class="label-l">品牌商：</td>
-                                <td valign="top" class="text-l"> 暂无数据</td>
-                            </tr>
-                            <tr>
-                                <td class="label-l">楼盘地址：</td>
-                                <td valign="top" class="text-l">北马路与西南河交汇处</td>
-                            </tr>
-                            </tbody>
-                        </table>
+    <div style="margin: 100px">
+        <div style="margin-top: 55px">
+            <Row :gutter="30">
+                <Col span="12">
+                    <div style="border: 1px solid #dcdee2;">
+                        <Row>
+                            <Col span="12">
+                                <div style="padding: 5px">
+                                    <span style="font-size: 14px"><strong>楼盘首页:</strong></span>
+                                    <span style="font-size: 14px"><strong>山水人家</strong></span>
+                                    <img style="height: 250px;width: 300px;margin: 10px"
+                                         src="../../../assets/img/44b8812c-cb76-4ac6-aa53-5f3ab4db1100.png" alt="">
+                                </div>
+                            </Col>
+                            <Col span="12">
+                                <div style="margin-top:40px;margin-left: 10px">
+                                    <div>
+                                        <div class="key"><strong>楼盘最低价:</strong></div>
+                                        <div class="value light"><span class="price">37000</span> 元/平方米
+                                        </div>
+                                        <Form :model="formItem" :label-width="70">
+                                            <FormItem label="价格说明:">
+                                                <span>价格为37000元/平-39000元/平</span>
+                                            </FormItem>
+                                            <FormItem label="楼盘位置:">
+                                                <span>大兴 庞各庄桥西1500米</span>
+                                            </FormItem>
+                                            <FormItem label="开盘时间:">
+                                                <span>2018年10月30日</span>
+                                            </FormItem>
+                                            <FormItem label="主力户型:">
+                                                <span>三居(建面89.0㎡)</span>
+                                            </FormItem>
+                                            <FormItem label="开发商:">
+                                            <span>北京中海兴达房地产开发有限公司</span>
+                                            </FormItem>
+                                        </Form>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
                     </div>
-                </TabPane>
-                <TabPane label="户型" name="name3">
-                </TabPane>
-                <TabPane label="楼盘点评" name="name4">
-                </TabPane>
-            </Tabs>
+                </Col>
+                <Col span="12">
+                    <div style="border: 1px solid #dcdee2;">
+                        <Row>
+                            <Col span="12">
+                                <div style="padding: 5px">
+                                    <span style="font-size: 14px"><strong>户型:</strong></span>
+                                    <span style="font-size: 14px"><strong>{{this.detailList[0].imagename}}</strong></span>
+                                    <img style="height: 250px;width: 300px;margin: 10px"
+                                        :src=detailList[0].image  alt="">
+                                </div>
+                            </Col>
+                            <Col span="12">
+                                <table style="margin-top: 50px">
+                                    <tbody>
+                                    <tr>
+                                        <td class="label-l">居室：</td>
+                                        <td valign="top" class="text-l"> {{this.detailList[0].imagename}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="label-l">层高：</td>
+                                        <td valign="top" class="text-l"> 暂无</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="label-l">户型朝向：</td>
+                                        <td valign="top" class="text-l"> 南</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="label-l">建筑面积：</td>
+                                        <td valign="top" class="text-l"> 74.0平米</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="label-l">详情：</td>
+                                        <td valign="top" class="text-l">{{this.detailList[0].charact}}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </Col>
+                        </Row>
+                    </div>
+
+                </Col>
+            </Row>
+            <br>
+            <Row :gutter="30">
+                <Col span="12">
+                    <div style="border: 1px solid #dcdee2;height: 280px">
+                        <div style="padding: 14px">
+                            <h3>基本信息</h3>
+                            <table class="table-noline">
+                                <tbody>
+                                <tr>
+                                    <td class="label-l">楼盘名称：</td>
+                                    <td valign="top" class="text-l"> 海上明珠</td>
+                                </tr>
+                                <tr>
+                                    <td class="label-l">物业类型：</td>
+                                    <td valign="top" class="text-l"> 普通住宅 , 自住型商品房</td>
+                                    <td class="label-r">容积率：</td>
+                                    <td valign="top" class="text-r">30%</td>
+                                </tr>
+                                <tr>
+                                    <td class="label-l">建筑类型：</td>
+                                    <td valign="top" class="text-l"> 板楼 , 高层</td>
+                                    <td class="label-r">绿化率：</td>
+                                    <td valign="top" class="text-r">30%</td>
+                                </tr>
+                                <tr>
+                                    <td class="label-l">产权年限：</td>
+                                    <td valign="top" class="text-l">70年</td>
+                                    <td class="label-r">装修情况：</td>
+                                    <td valign="top" class="text-r"> 毛坯</td>
+                                </tr>
+                                <tr>
+                                    <td class="label-l">楼盘地址：</td>
+                                    <td valign="top" class="text-l">北马路与西南河交汇处</td>
+                                    <td class="label-r">建筑面积：</td>
+                                    <td valign="top" class="text-r">30000</td>
+
+                                </tr>
+                                <tr>
+                                    <td class="label-l">产权年限：</td>
+                                    <td valign="top" class="text-l">70年</td>
+                                    <td class="label-r">装修情况：</td>
+                                    <td valign="top" class="text-r"> 毛坯</td>
+                                </tr>
+                                <tr>
+                                    <td class="label-l">楼盘地址：</td>
+                                    <td valign="top" class="text-l">北马路与西南河交汇处</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </Col>
+                <Col span="12">
+                    <div style="border: 1px solid #dcdee2;">
+                        <div style="padding: 14px;height: 280px;overflow-y: scroll">
+                            <h3>楼盘评论</h3>
+                            <Form :model="formItem" :label-width="70">
+                                <FormItem label="焦点网友:">
+                                    <span>在园中植栽设计上，采用色彩丰富、复合种植、自然搭配的手法，树阵、花树搭配修剪成型的球类及绿篱错落有致，园中春夏秋冬，四季有景有保障中海项目，大企业，有保障中海项目，大企业，有保障中海项目，大企业，有保障</span>
+                                </FormItem>
+                                <FormItem label="焦点网友:">
+                                    <span>在园中植栽设计上，采用色彩丰富、复合种植、自然搭配的手法，树阵、花树搭配修剪成型的球类及绿篱错落有致，园中春夏秋冬，四季有景有保障中海项目，大企业，有保障中海项目，大企业，有保障中海项目，大企业，有保障</span>
+                                </FormItem>
+                                <FormItem label="焦点网友:">
+                                    <span>在园中植栽设计上，采用色彩丰富、复合种植、自然搭配的手法，树阵、花树搭配修剪成型的球类及绿篱错落有致</span>
+                                </FormItem>
+                            </Form>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
         </div>
     </div>
 </template>
 
 <script>
+    import Server from "@/core/server";
+    import {services} from "@/core/config/services";
     export default {
-        name: "houseinfo"
+        mounted() {
+          this.getimg();
+        },
+        data(){
+            return{
+                detailList:[]
+            }
+        },
+        methods:{
+            getimg() {
+                var _this = this;
+                Server.get({
+                    url: services.getImageToCommunity
+                }).then(rsp => {
+                    if (rsp.status === 1) {
+                        _this.detailList = rsp.imageData;
+                    }
+                });
+                // Server.get({
+                //     url: services.compareHouseDetails,
+                //     params: {
+                //         name: this.houseName[0]
+                //     }
+                // }).then(function (rsp) {
+                //     if (rsp.status === 1) {
+                //
+                //     } else if (rsp.errMsg === "null") {
+                //
+                //     }
+                // });
+            },
+
+        },
     }
 </script>
 
-<style lang="less"  scoped>
+<style lang="less" scoped>
+    .value {
+        display: inline-block;
+        zoom: 1;
+        _zoom: 1;
+        *display: inline;
+        padding-left: 8px;
+    }
+
+    .price {
+        color: #e43;
+        font-size: 30px;
+        font-weight: 700;
+        vertical-align: baseline;
+        margin-right: 5px;
+    }
+
+    .key {
+        font-size: 14px;
+        display: inline-block;
+        white-space: nowrap;
+    }
+
     .label-l {
         min-width: 70px;
         color: #666;
@@ -80,6 +232,7 @@
         padding: 5px 0;
         line-height: 24px;
     }
+
     .label-r {
         width: 74px;
         text-align: right;
@@ -89,25 +242,41 @@
         padding: 5px 0;
         line-height: 24px;
     }
+
     .text-l {
         width: 366px;
         line-height: 24px;
         vertical-align: top;
         padding: 5px 40px 5px 4px;
     }
+
     .text-r {
         width: 286px;
         line-height: 24px;
         overflow: hidden;
         display: inline-block;
     }
+
     tr {
         display: table-row;
         vertical-align: inherit;
         border-color: inherit;
     }
-    td{
+
+    td {
         margin: 5px;
     }
 
+    .ivu-form-item {
+        margin-bottom: 1px;
+    }
+
+    .ivu-divider-horizontal {
+        display: block;
+        height: 1px;
+        width: 100%;
+        min-width: 100%;
+        margin: 15px 0;
+        clear: both;
+    }
 </style>
