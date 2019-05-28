ad6441aea6761f1003e352ad8a3f2b7509478e3e
gis325 <3407452861@qq.com>
2019-05-28 10:56:19 +0800

    jz

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import main from '@/views/main/index.vue'
 import login from './views/login/index.vue'
-import test from './views/test/heat.vue'
+import test from './views/test/test.vue'
 Vue.use(Router)
 
 export default new Router({
