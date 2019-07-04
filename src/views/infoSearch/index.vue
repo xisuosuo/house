cecc322166974b8d377f8f7b918ca5c1c75d59fe
gis325 <3407452861@qq.com>
2019-07-04 21:43:47 +0800

    jz

--- src/views/infoSearch/index.vue
+++ src/views/infoSearch/index.vue
@@ -5,8 +5,8 @@
         <Menu active-name="" theme="dark" width="auto" :open-names="['']">
           <router-link to="/menu">
             <MenuItem name="1-1">
-              <Icon type="ios-navigate"></Icon>
-              <span>首页</span>
+            <Icon type="ios-navigate"></Icon>
+            <span>首页</span>
             </MenuItem>
           </router-link>
           <Submenu name="2">
@@ -15,12 +15,12 @@
             </template>
             <router-link to="/data">
               <MenuItem name="2-1">
-                <span style="font-size: 13px">数据统计</span>
+              <span style="font-size: 13px">数据统计</span>
               </MenuItem>
             </router-link>
             <router-link to="/data/dataDeatils">
               <MenuItem name="2-2">
-                <span style="font-size: 13px">数据查询</span>
+              <span style="font-size: 13px">数据查询</span>
               </MenuItem>
             </router-link>
           </Submenu>
@@ -29,21 +29,21 @@
               <Icon type="md-search" /> 信息查询
             </template>
             <MenuItem name="3-1">
-              <span @click="siteSelect">条件选房</span>
+            <span @click="siteSelect">条件选房</span>
             </MenuItem>
             <Submenu name="3-1-1">
               <template slot="title">
                 数据统计
               </template>
               <MenuItem name="3-1-1-1">
-                <span @click="selectHouse() ">公共设施</span>
+              <span @click="selectHouse() ">公共设施</span>
               </MenuItem>
               <MenuItem name="3-1-1-2">
-                <span @click="selectBuffer() ">缓冲区分析</span>
+              <span @click="selectBuffer() ">缓冲区分析</span>
               </MenuItem>
             </Submenu>
             <MenuItem name="3-3">
-              <span @click="controlAnlayse">周边设施</span>
+            <span @click="controlAnlayse">周边设施</span>
             </MenuItem>
           </Submenu>
           <Submenu name="4">
@@ -52,35 +52,35 @@
             </template>
             <router-link to="/locationResources">
               <MenuItem name="4-1">
-                <span>学区资源</span>
+              <span>学区资源</span>
               </MenuItem>
             </router-link>
             <router-link to="/medicalResources">
               <MenuItem name="4-2">
-                <span>医疗资源</span>
+              <span>医疗资源</span>
               </MenuItem>
             </router-link>
             <router-link to="/greenlResources">
               <MenuItem name="4-3">
-                <span>绿地资源</span>
+              <span>绿地资源</span>
               </MenuItem>
             </router-link>
             <router-link to="/businessResources">
               <MenuItem name="4-4">
-                <span>商业资源</span>
+              <span>商业资源</span>
               </MenuItem>
             </router-link>
           </Submenu>
           <router-link to="/recommendation/factor">
             <MenuItem name="5-1">
-              <Icon type="md-pin" />
-              <span>购房选址</span>
+            <Icon type="md-pin" />
+            <span>购房选址</span>
             </MenuItem>
           </router-link>
           <router-link to="/recommendation/housingPreference">
             <MenuItem name="6-1">
-              <Icon type="md-thumbs-up" />
-              <span>房源推荐</span>
+            <Icon type="md-thumbs-up" />
+            <span>房源推荐</span>
             </MenuItem>
           </router-link>
 
@@ -90,17 +90,17 @@
             </template>
             <router-link to="/priceAnalysis">
               <MenuItem name="7-1">
-                <span style="font-size: 13px">价格分析</span>
+              <span style="font-size: 13px">价格分析</span>
               </MenuItem>
             </router-link>
             <router-link to="/heatMap2d">
               <MenuItem name="7-2">
-                <span style="font-size: 13px">二维热力图</span>
+              <span style="font-size: 13px">二维热力图</span>
               </MenuItem>
             </router-link>
             <router-link to="/heatMap">
               <MenuItem name="7-3">
-                <span style="font-size: 13px">三维热力图</span>
+              <span style="font-size: 13px">三维热力图</span>
               </MenuItem>
             </router-link>
           </Submenu>
@@ -108,26 +108,27 @@
             <template slot="title">
               <Icon type="md-analytics" /> 房价预测
             </template>
-            <router-link to="/Kriging">
-              <MenuItem name="8-1">
-                <span style="font-size: 13px">克里金插值预测</span>
-              </MenuItem>
-            </router-link>
             <router-link to="/GWR">
               <MenuItem name="8-2">
-                <span style="font-size: 13px">GWR模型</span>
+              <span style="font-size: 13px">GWR模型</span>
               </MenuItem>
             </router-link>
+            <router-link to="/Kriging">
+              <MenuItem name="8-1">
+              <span style="font-size: 13px">克里金插值预测</span>
+              </MenuItem>
+            </router-link>
+
             <router-link to="/priceForecast">
               <MenuItem name="8-3">
-                <span style="font-size: 13px">房价预测</span>
+              <span style="font-size: 13px">房价预测</span>
               </MenuItem>
             </router-link>
           </Submenu>
           <router-link to="/userManager">
             <MenuItem name="9-1" v-if="show">
-              <Icon type="md-contacts" />
-              <span>用户管理</span>
+            <Icon type="md-contacts" />
+            <span>用户管理</span>
             </MenuItem>
           </router-link>
           <Submenu name="10">
@@ -136,24 +137,24 @@
             </template>
             <router-link to="/auDecision/usersManual">
               <MenuItem name="10-1">
-                <span style="font-size: 13px">用户手册</span>
+              <span style="font-size: 13px">用户手册</span>
               </MenuItem>
             </router-link>
             <router-link to="/auDecision/livablemodel">
               <MenuItem name="10-2">
-                <span style="font-size: 13px">宜居模型</span>
+              <span style="font-size: 13px">宜居模型</span>
               </MenuItem>
             </router-link>
             <router-link to="/auDecision/model">
               <MenuItem name="10-3">
-                <span style="font-size: 13px">引力模型</span>
+              <span style="font-size: 13px">引力模型</span>
               </MenuItem>
             </router-link>
           </Submenu>
           <router-link to="/personalcenter">
             <MenuItem name="11-1">
-              <Icon type="md-person" />
-              <span>个人中心</span>
+            <Icon type="md-person" />
+            <span>个人中心</span>
             </MenuItem>
           </router-link>
         </Menu>
@@ -161,7 +162,7 @@
       <Content>
         <Header :style="{padding: 0}" class="layout-header-bar">
           <Breadcrumb>
-            <Icon  :style="{margin: '0 5px'}" type="md-menu" size="25"></Icon>
+            <Icon :style="{margin: '0 5px'}" type="md-menu" size="25"></Icon>
             <BreadcrumbItem v-for="(item,idx) in $route.matched" :key="idx" :to="(item.path)">{{item.name}}</BreadcrumbItem>
           </Breadcrumb>
         </Header>
--- src/views/recommendation/recommendHousing.vue
+++ src/views/recommendation/recommendHousing.vue
@@ -1,324 +1,325 @@
 <template>
   <Layout :style="{height:'100%'}">
-  <Sider ref="side1" :width='180'>
-    <Menu active-name="" theme="dark" width="auto" :open-names="['']">
-      <router-link to="/menu">
-        <MenuItem name="1-1">
-        <Icon type="ios-navigate"></Icon>
-        <span>首页</span>
-        </MenuItem>
-      </router-link>
-      <Submenu name="2">
-        <template slot="title">
-          <Icon type="md-speedometer" /> 数据展示
-        </template>
-        <router-link to="/data">
-          <MenuItem name="2-1">
-          <span style="font-size: 13px">数据统计</span>
+    <Sider ref="side1" :width='180'>
+      <Menu active-name="" theme="dark" width="auto" :open-names="['']">
+        <router-link to="/menu">
+          <MenuItem name="1-1">
+          <Icon type="ios-navigate"></Icon>
+          <span>首页</span>
           </MenuItem>
         </router-link>
-        <router-link to="/data/dataDeatils">
-          <MenuItem name="2-2">
-          <span style="font-size: 13px">数据查询</span>
+        <Submenu name="2">
+          <template slot="title">
+            <Icon type="md-speedometer" /> 数据展示
+          </template>
+          <router-link to="/data">
+            <MenuItem name="2-1">
+            <span style="font-size: 13px">数据统计</span>
+            </MenuItem>
+          </router-link>
+          <router-link to="/data/dataDeatils">
+            <MenuItem name="2-2">
+            <span style="font-size: 13px">数据查询</span>
+            </MenuItem>
+          </router-link>
+        </Submenu>
+        <router-link to="/infoSearch">
+          <MenuItem name="3-1">
+          <Icon type="md-search" />
+          <span>信息查询</span>
           </MenuItem>
         </router-link>
-      </Submenu>
-      <router-link to="/infoSearch">
-        <MenuItem name="3-1">
-        <Icon type="md-search" />
-        <span>信息查询</span>
-        </MenuItem>
-      </router-link>
-      <Submenu name="4">
-        <template slot="title">
-          <Icon type="md-globe" /> 区位资源
-        </template>
-        <router-link to="/locationResources">
-          <MenuItem name="4-1">
-          <span>学区资源</span>
+        <Submenu name="4">
+          <template slot="title">
+            <Icon type="md-globe" /> 区位资源
+          </template>
+          <router-link to="/locationResources">
+            <MenuItem name="4-1">
+            <span>学区资源</span>
+            </MenuItem>
+          </router-link>
+          <router-link to="/medicalResources">
+            <MenuItem name="4-2">
+            <span>医疗资源</span>
+            </MenuItem>
+          </router-link>
+          <router-link to="/greenlResources">
+            <MenuItem name="4-3">
+            <span>绿地资源</span>
+            </MenuItem>
+          </router-link>
+          <router-link to="/businessResources">
+            <MenuItem name="4-4">
+            <span>商业资源</span>
+            </MenuItem>
+          </router-link>
+        </Submenu>
+        <router-link to="/recommendation/factor">
+          <MenuItem name="5-1">
+          <Icon type="md-pin" />
+          <span>购房选址</span>
           </MenuItem>
         </router-link>
-        <router-link to="/medicalResources">
-          <MenuItem name="4-2">
-          <span>医疗资源</span>
+        <router-link to="/recommendation/housingPreference">
+          <MenuItem name="6-1">
+          <Icon type="md-thumbs-up" />
+          <span>房源推荐</span>
           </MenuItem>
         </router-link>
-        <router-link to="/greenlResources">
-          <MenuItem name="4-3">
-          <span>绿地资源</span>
-          </MenuItem>
-        </router-link>
-        <router-link to="/businessResources">
-          <MenuItem name="4-4">
-          <span>商业资源</span>
-          </MenuItem>
-        </router-link>
-      </Submenu>
-      <router-link to="/recommendation/factor">
-        <MenuItem name="5-1">
-        <Icon type="md-pin" />
-        <span>购房选址</span>
-        </MenuItem>
-      </router-link>
-      <router-link to="/recommendation/housingPreference">
-        <MenuItem name="6-1">
-        <Icon type="md-thumbs-up" />
-        <span>房源推荐</span>
-        </MenuItem>
-      </router-link>
 
-      <Submenu name="7">
-        <template slot="title">
-          <Icon type="ios-radio" /> 房价分析
-        </template>
-        <router-link to="/priceAnalysis">
-          <MenuItem name="7-1">
-          <span style="font-size: 13px">价格分析</span>
-          </MenuItem>
-        </router-link>
-        <router-link to="/heatMap2d">
-          <MenuItem name="7-2">
-          <span style="font-size: 13px">二维热力图</span>
-          </MenuItem>
-        </router-link>
-        <router-link to="/heatMap">
-          <MenuItem name="7-3">
-          <span style="font-size: 13px">三维热力图</span>
-          </MenuItem>
-        </router-link>
-      </Submenu>
-      <Submenu name="8">
-        <template slot="title">
-          <Icon type="md-analytics" /> 房价预测
-        </template>
-        <router-link to="/Kriging">
-          <MenuItem name="8-1">
-          <span style="font-size: 13px">克里金插值预测</span>
-          </MenuItem>
-        </router-link>
-        <router-link to="/GWR">
-          <MenuItem name="8-2">
-          <span style="font-size: 13px">GWR模型</span>
-          </MenuItem>
-        </router-link>
-        <router-link to="/priceForecast">
-          <MenuItem name="8-3">
-          <span style="font-size: 13px">房价预测</span>
-          </MenuItem>
-        </router-link>
-      </Submenu>
-      <router-link to="/userManager">
-        <MenuItem name="9-1" v-if="show">
-        <Icon type="md-contacts" />
-        <span>用户管理</span>
-        </MenuItem>
-      </router-link>
-      <Submenu name="10">
-        <template slot="title">
-          <Icon type="md-paper" /> 用户手册
-        </template>
-        <router-link to="/auDecision/usersManual">
-          <MenuItem name="10-1">
-          <span style="font-size: 13px">用户手册</span>
-          </MenuItem>
-        </router-link>
-        <router-link to="/auDecision/livablemodel">
-          <MenuItem name="10-2">
-          <span style="font-size: 13px">宜居模型</span>
-          </MenuItem>
-        </router-link>
-        <router-link to="/auDecision/model">
-          <MenuItem name="10-3">
-          <span style="font-size: 13px">引力模型</span>
-          </MenuItem>
-        </router-link>
-      </Submenu>
-      <router-link to="/personalcenter">
-        <MenuItem name="11-1">
-        <Icon type="md-person" />
-        <span>个人中心</span>
-        </MenuItem>
-      </router-link>
-    </Menu>
-  </Sider>
+        <Submenu name="7">
+          <template slot="title">
+            <Icon type="ios-radio" /> 房价分析
+          </template>
+          <router-link to="/priceAnalysis">
+            <MenuItem name="7-1">
+            <span style="font-size: 13px">价格分析</span>
+            </MenuItem>
+          </router-link>
+          <router-link to="/heatMap2d">
+            <MenuItem name="7-2">
+            <span style="font-size: 13px">二维热力图</span>
+            </MenuItem>
+          </router-link>
+          <router-link to="/heatMap">
+            <MenuItem name="7-3">
+            <span style="font-size: 13px">三维热力图</span>
+            </MenuItem>
+          </router-link>
+        </Submenu>
+        <Submenu name="8">
+          <template slot="title">
+            <Icon type="md-analytics" /> 房价预测
+          </template>
+          <router-link to="/GWR">
+            <MenuItem name="8-2">
+            <span style="font-size: 13px">GWR模型</span>
+            </MenuItem>
+          </router-link>
+          <router-link to="/Kriging">
+            <MenuItem name="8-1">
+            <span style="font-size: 13px">克里金插值预测</span>
+            </MenuItem>
+          </router-link>
 
-  <Content>
-    <div style="margin:65px auto;width:1000px;">
-      <table id="customers">
-        <thead>
-          <tr>
-            <th>小区名称</th>
-            <th @click="education" title="查看教育资源可达性计算数据">教育资源可达性
-              <Icon type="ios-book" />
-            </th>
-            <th @click="Medical" title="查看医疗资源可达性计算数据">医疗资源可达性
-              <Icon type="md-add-circle" />
-            </th>
-            <th @click="traffic" title="查看交通资源可达性计算数据">交通资源可达性
-              <Icon type="ios-car" />
-            </th>
-            <th @click="entertainment" title="查看休闲娱乐可达性计算数据">休闲娱乐可达性
-              <Icon type="md-cart" />
-            </th>
-            <th>综合值</th>
-          </tr>
-        </thead>
-        <tbody id="customerss"></tbody>
-      </table>
-      <Row>
-        <Col span="12">
-        <div id="trend" style="height: 350px;margin-top: 5px;margin-right: 5px; border: 1px solid  #dcdee2">
-        </div>
-        </Col>
-        <Col span="12">
-        <div id="comprise" style="height:350px;margin-top: 5px; margin-left: 5px;border: 1px solid  #dcdee2">
-        </div>
-        </Col>
-      </Row>
-      <Row type="flex" justify="end" class="code-row-bg" style="margin-top:6px">
-        <Col span="5">
-        <ButtonGroup shape="circle">
-          <Button type="primary" @click="back()">
-            <Icon type="ios-arrow-back"></Icon>
-            重新推荐
-          </Button>
-          <Button type="primary" @click="map()">
-            地图查看
-            <Icon type="ios-arrow-forward"></Icon>
-          </Button>
-        </ButtonGroup>
-        </Col>
-      </Row>
+          <router-link to="/priceForecast">
+            <MenuItem name="8-3">
+            <span style="font-size: 13px">房价预测</span>
+            </MenuItem>
+          </router-link>
+        </Submenu>
+        <router-link to="/userManager">
+          <MenuItem name="9-1" v-if="show">
+          <Icon type="md-contacts" />
+          <span>用户管理</span>
+          </MenuItem>
+        </router-link>
+        <Submenu name="10">
+          <template slot="title">
+            <Icon type="md-paper" /> 用户手册
+          </template>
+          <router-link to="/auDecision/usersManual">
+            <MenuItem name="10-1">
+            <span style="font-size: 13px">用户手册</span>
+            </MenuItem>
+          </router-link>
+          <router-link to="/auDecision/livablemodel">
+            <MenuItem name="10-2">
+            <span style="font-size: 13px">宜居模型</span>
+            </MenuItem>
+          </router-link>
+          <router-link to="/auDecision/model">
+            <MenuItem name="10-3">
+            <span style="font-size: 13px">引力模型</span>
+            </MenuItem>
+          </router-link>
+        </Submenu>
+        <router-link to="/personalcenter">
+          <MenuItem name="11-1">
+          <Icon type="md-person" />
+          <span>个人中心</span>
+          </MenuItem>
+        </router-link>
+      </Menu>
+    </Sider>
 
-      <Modal v-model="modal1" title="教育资源的中间过程数据" :styles="{top: '55px'}" width="700">
+    <Content>
+      <div style="margin:65px auto;width:1000px;">
         <table id="customers">
           <thead>
             <tr>
               <th>小区名称</th>
-              <th>人口</th>
+              <th @click="education" title="查看教育资源可达性计算数据">教育资源可达性
+                <Icon type="ios-book" />
+              </th>
+              <th @click="Medical" title="查看医疗资源可达性计算数据">医疗资源可达性
+                <Icon type="md-add-circle" />
+              </th>
+              <th @click="traffic" title="查看交通资源可达性计算数据">交通资源可达性
+                <Icon type="ios-car" />
+              </th>
+              <th @click="entertainment" title="查看休闲娱乐可达性计算数据">休闲娱乐可达性
+                <Icon type="md-cart" />
+              </th>
+              <th>综合值</th>
             </tr>
           </thead>
-          <tbody id="population"></tbody>
+          <tbody id="customerss"></tbody>
         </table>
+        <Row>
+          <Col span="12">
+          <div id="trend" style="height: 350px;margin-top: 5px;margin-right: 5px; border: 1px solid  #dcdee2">
+          </div>
+          </Col>
+          <Col span="12">
+          <div id="comprise" style="height:350px;margin-top: 5px; margin-left: 5px;border: 1px solid  #dcdee2">
+          </div>
+          </Col>
+        </Row>
+        <Row type="flex" justify="end" class="code-row-bg" style="margin-top:6px">
+          <Col span="5">
+          <ButtonGroup shape="circle">
+            <Button type="primary" @click="back()">
+              <Icon type="ios-arrow-back"></Icon>
+              重新推荐
+            </Button>
+            <Button type="primary" @click="map()">
+              地图查看
+              <Icon type="ios-arrow-forward"></Icon>
+            </Button>
+          </ButtonGroup>
+          </Col>
+        </Row>
 
-        <table id="customers">
-          <thead>
-            <tr>
-              <th>公服设施</th>
-              <th>服务能力</th>
-            </tr>
-          </thead>
-          <tbody id="service"></tbody>
-        </table>
+        <Modal v-model="modal1" title="教育资源的中间过程数据" :styles="{top: '55px'}" width="700">
+          <table id="customers">
+            <thead>
+              <tr>
+                <th>小区名称</th>
+                <th>人口</th>
+              </tr>
+            </thead>
+            <tbody id="population"></tbody>
+          </table>
 
-        <table id="customers">
-          <thead>
-            <tr>
-              <th></th>
-              <th>距离</th>
-            </tr>
-          </thead>
-          <tbody id="distance"></tbody>
-        </table>
-      </Modal>
+          <table id="customers">
+            <thead>
+              <tr>
+                <th>公服设施</th>
+                <th>服务能力</th>
+              </tr>
+            </thead>
+            <tbody id="service"></tbody>
+          </table>
 
-      <Modal v-model="modal2" title="医疗资源的中间过程数据" :styles="{top: '55px'}" width="700">
-        <table id="customers">
-          <thead>
-            <tr>
-              <th>小区名称</th>
-              <th>人口</th>
-            </tr>
-          </thead>
-          <tbody id="population2"></tbody>
-        </table>
+          <table id="customers">
+            <thead>
+              <tr>
+                <th></th>
+                <th>距离</th>
+              </tr>
+            </thead>
+            <tbody id="distance"></tbody>
+          </table>
+        </Modal>
 
-        <table id="customers">
-          <thead>
-            <tr>
-              <th></th>
-              <th>服务能力</th>
-            </tr>
-          </thead>
-          <tbody id="service2"></tbody>
-        </table>
+        <Modal v-model="modal2" title="医疗资源的中间过程数据" :styles="{top: '55px'}" width="700">
+          <table id="customers">
+            <thead>
+              <tr>
+                <th>小区名称</th>
+                <th>人口</th>
+              </tr>
+            </thead>
+            <tbody id="population2"></tbody>
+          </table>
 
-        <table id="customers">
-          <thead>
-            <tr>
-              <th></th>
-              <th>距离</th>
-            </tr>
-          </thead>
-          <tbody id="distance2"></tbody>
-        </table>
-      </Modal>
+          <table id="customers">
+            <thead>
+              <tr>
+                <th></th>
+                <th>服务能力</th>
+              </tr>
+            </thead>
+            <tbody id="service2"></tbody>
+          </table>
 
-      <Modal v-model="modal3" title="交通资源的中间过程数据" :styles="{top: '55px'}" width="700">
-        <table id="customers">
-          <thead>
-            <tr>
-              <th>小区名称</th>
-              <th>人口</th>
-            </tr>
-          </thead>
-          <tbody id="population3"></tbody>
-        </table>
+          <table id="customers">
+            <thead>
+              <tr>
+                <th></th>
+                <th>距离</th>
+              </tr>
+            </thead>
+            <tbody id="distance2"></tbody>
+          </table>
+        </Modal>
 
-        <table id="customers">
-          <thead>
-            <tr>
-              <th></th>
-              <th>服务能力</th>
-            </tr>
-          </thead>
-          <tbody id="service3"></tbody>
-        </table>
+        <Modal v-model="modal3" title="交通资源的中间过程数据" :styles="{top: '55px'}" width="700">
+          <table id="customers">
+            <thead>
+              <tr>
+                <th>小区名称</th>
+                <th>人口</th>
+              </tr>
+            </thead>
+            <tbody id="population3"></tbody>
+          </table>
 
-        <table id="customers">
-          <thead>
-            <tr>
-              <th></th>
-              <th>距离</th>
-            </tr>
-          </thead>
-          <tbody id="distance3"></tbody>
-        </table>
-      </Modal>
-      <Modal v-model="modal4" title="休闲娱乐的中间过程数据" :styles="{top: '55px'}" width="700">
-        <table id="customers">
-          <thead>
-            <tr>
-              <th>小区名称</th>
-              <th>人口</th>
-            </tr>
-          </thead>
-          <tbody id="population4"></tbody>
-        </table>
+          <table id="customers">
+            <thead>
+              <tr>
+                <th></th>
+                <th>服务能力</th>
+              </tr>
+            </thead>
+            <tbody id="service3"></tbody>
+          </table>
 
-        <table id="customers">
-          <thead>
-            <tr>
-              <th></th>
-              <th>服务能力</th>
-            </tr>
-          </thead>
-          <tbody id="service4"></tbody>
-        </table>
+          <table id="customers">
+            <thead>
+              <tr>
+                <th></th>
+                <th>距离</th>
+              </tr>
+            </thead>
+            <tbody id="distance3"></tbody>
+          </table>
+        </Modal>
+        <Modal v-model="modal4" title="休闲娱乐的中间过程数据" :styles="{top: '55px'}" width="700">
+          <table id="customers">
+            <thead>
+              <tr>
+                <th>小区名称</th>
+                <th>人口</th>
+              </tr>
+            </thead>
+            <tbody id="population4"></tbody>
+          </table>
 
-        <table id="customers">
-          <thead>
-            <tr>
-              <th></th>
-              <th>距离</th>
-            </tr>
-          </thead>
-          <tbody id="distance4"></tbody>
-        </table>
-      </Modal>
+          <table id="customers">
+            <thead>
+              <tr>
+                <th></th>
+                <th>服务能力</th>
+              </tr>
+            </thead>
+            <tbody id="service4"></tbody>
+          </table>
 
-    </div>
-  </Content>
+          <table id="customers">
+            <thead>
+              <tr>
+                <th></th>
+                <th>距离</th>
+              </tr>
+            </thead>
+            <tbody id="distance4"></tbody>
+          </table>
+        </Modal>
+
+      </div>
+    </Content>
   </Layout>
 </template>
 <script>
