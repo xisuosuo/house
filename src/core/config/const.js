4267771fd99554d18cbf0353843193f1be292aa5
xisuosuo <1315270706@qq.com>
2019-07-05 15:51:23 +0800

    2
    2

--- src/core/config/const.js
+++ src/core/config/const.js
@@ -1,19 +1,26 @@
-export const MapAPI = {
-  js: "http://localhost/arcgis_js_v48_sdk/arcgis_js_api/library/4.8/init.js",
-  css: "http://localhost/arcgis_js_v48_sdk/arcgis_js_api/library/4.8/esri/css/main.css"
-}; //朱震
-
-
 // export const MapAPI = {
-//   js: "http://localhost/arcgis_js_v410_sdk/arcgis_js_api/library/4.10/init.js",
-//   css: "http://localhost/arcgis_js_v410_sdk/arcgis_js_api/library/4.10/esri/css/main.css"
-// };
+//   js: "http://localhost/arcgis_js_v48_sdk/arcgis_js_api/library/4.8/init.js",
+//   css: "http://localhost/arcgis_js_v48_sdk/arcgis_js_api/library/4.8/esri/css/main.css"
+// }; //朱震
+<<<<<<< HEAD
 
 
+export const MapAPI = {
+  js: "http://122.112.216.247:8089/arcgis_js_v410_sdk/arcgis_js_api/library/4.10/init.js",
+  css: "http://122.112.216.247:8089/arcgis_js_v410_sdk/arcgis_js_api/library/4.10/esri/css/main.css"
+};
+
+//
 // export const MapAPI = {
 //   js: "http://localhost/arcgis_js_v48_sdk/arcgis_js_v48_sdk/arcgis_js_api/library/4.8/init.js",
 //   css: "http://localhost/arcgis_js_v48_sdk/arcgis_js_v48_sdk/arcgis_js_api/library/4.8/esri/css/main.css"
 // };  //天翔
+=======
+export const MapAPI = {
+  js: "http://58.210.9.131/esriJS/4.7/",
+  css: "http://58.210.9.131/esriJS/4.7/esri/css/main.css"
+}; //官网
+>>>>>>> b501268d6abad04d701781a36b88e7f941482869
 
 export const IP = "122.112.216.247:8080"; //正式
 // export const IP = "192.168.1.101:8080"; //测试
